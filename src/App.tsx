75fa72f17e9bee64cabc5bc2b5fcf4b34e8fc8dc
Ezequiel Gonzalez <egonzalez@EzequielsLaptop.fibertel.com.ar>
2024-05-14 23:22:53 -0300

    Added selectors to App and a WIP context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import ChaptersTable from "./components/ChaptersTable";
 import DualCharactersList from "./components/DualCharactersList";
+// import { CharacterSelectionProvider } from "./context";
+import type { Character } from "./types/ApiTypes";
 
 function App() {
+	const [selectedCharacter1, setSelectedCharacter1] =
+		useState<Character | null>(null);
+	const [selectedCharacter2, setSelectedCharacter2] =
+		useState<Character | null>(null);
+
 	return (
+		// <CharacterSelectionProvider>
 		<>
 			<h1>Rick and Morty App!</h1>
-			<DualCharactersList />
+			<DualCharactersList
+				selectedCharacter1={selectedCharacter1}
+				selectedCharacter2={selectedCharacter2}
+				setSelectedCharacter1={setSelectedCharacter1}
+				setSelectedCharacter2={setSelectedCharacter2}
+			/>
+			<ChaptersTable
+				selectedCharacter1={selectedCharacter1}
+				selectedCharacter2={selectedCharacter2}
+			/>
 		</>
+		// </CharacterSelectionProvider>
 	);
 }
 
